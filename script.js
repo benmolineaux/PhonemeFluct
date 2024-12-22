a753f47f5ccf233e1458bee1bd0fbaa7f89f313a
Benjamin Molineaux <benjamin.molineaux@ed.ac.uk>
2024-12-22 23:03:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function createLabel(text) {
         className: 'marker-label', // Add a custom class for styling
         html: `<span>${text}</span>`,
         iconSize: [100, 20], // Adjust size of the label container
-        iconAnchor: [0, 35] // Adjust position relative to the marker
+        iconAnchor: [5, 40] // Adjust position relative to the marker
     });
 }
 // Custom red marker icon
